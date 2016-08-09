11a8923fd659a9b4e4cee9f055af098d1d0618f6
Alex Beals <alexcbeals@gmail.com>
2016-08-08 21:13:27 -0400

    switched to array tagging system

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -3,7 +3,7 @@ import Post from '../models/post_model';
 export const createPost = (req, res) => {
   const post = new Post();
   post.title = req.body.title;
-  post.tags = req.body.tags;
+  post.tags = req.body.tags.split(' ');
   post.content = req.body.content;
 
   post.save()
