c429dff2c50f25f0e36c1eee036df74a03201846
Alex Beals <alexcbeals@gmail.com>
2016-08-12 15:23:30 -0400

    finished user model

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -12,10 +12,12 @@ const UserSchema = new Schema(
   }
 );
 
+// Map _id to id
 UserSchema.set('toJSON', {
   virtuals: true,
 });
 
+// Handle password hashing
 UserSchema.pre('save', (next) => {
   const user = this;
 
@@ -36,6 +38,15 @@ UserSchema.pre('save', (next) => {
     });
   });
 });
+
+// Add password compare function
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+};
 
 // create model class
 const UserModel = mongoose.model('User', UserSchema);
