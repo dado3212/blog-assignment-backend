a7f2cf809be41cafa4d71c950e4a1c4a70b85eb7
Alex Beals <alexcbeals@gmail.com>
2016-08-12 15:21:50 -0400

    authentication modifications for user

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 // create a schema for posts
 const UserSchema = new Schema(
@@ -13,6 +14,27 @@ const UserSchema = new Schema(
 
 UserSchema.set('toJSON', {
   virtuals: true,
+});
+
+UserSchema.pre('save', (next) => {
+  const user = this;
+
+  // Ignore hashing if password not being modified
+  if (!user.isModified('password')) return next();
+
+  // generate salt with callback (async)
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      return next();
+    });
+  });
 });
 
 // create model class
